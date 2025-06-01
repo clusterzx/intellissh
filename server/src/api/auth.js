53f3ac19c27f85badebeb5262e0e767ac6066193
Clusterzx <clusterz@protonmail.com>
2025-06-01 16:12:03 +0200

    Increase rate limit login

--- server/src/api/auth.js
+++ server/src/api/auth.js
@@ -18,7 +18,7 @@ const authLimiter = rateLimit({
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3, // limit each IP to 3 login attempts per windowMs
+  max: 10, // limit each IP to 10 login attempts per windowMs
   message: {
     error: 'Too many login attempts, please try again later.'
   },
