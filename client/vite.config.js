8df2fd9682e234c9e31b702938a7c1fd82899ac1
KyoMio <szx0429@gmail.com>
2025-08-21 01:12:23 +0800

    Import vue-i18n

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import vueI18n from '@intlify/unplugin-vue-i18n/vite' 
 
 // https://vitejs.dev/config/
 export default defineConfig({
